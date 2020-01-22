1301d5e5fecaede59860e3b0b45738f3cdffc101
Mauricio Neto <maumneto@gmail.com>
2020-01-22 08:12:35 -0300

    update site

--- js/chart_pie.js
+++ js/chart_pie.js
@@ -3,7 +3,7 @@ var ctx = document.getElementById('type_pub').getContext('2d');
 var chart = new Chart(ctx, {
     type: 'doughnut',
     data:{
-        labels: ['Telecommunication', 'E-health', 'Wireless Sensor Network (WSN)', 'Cloud Computing', 'Teaching', 'Internet of Things (IoT)', 'Blockchain'],
+        labels: ['Telecommunication', 'e-Health', 'Wireless Sensor Network (WSN)', 'Cloud Computing', 'Teaching', 'Internet of Things (IoT)', 'Blockchain'],
         datasets:[{
             data: [5, 2, 2, 3, 3, 8, 1],
             backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850", '#87CEEB', 'orange']
